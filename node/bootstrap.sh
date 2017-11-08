628e90409ac64a0e993b64e45ba7d251b4af3df1
Hendrik Neumann <neumann.hendrik@gmail.com>
2017-11-08 23:00:43 +0100

    asd

--- node/bootstrap.sh
+++ node/bootstrap.sh
@@ -5,5 +5,5 @@ cd /home/pirate/raspberrycloud/
 git reset --hard
 git clean -fd
 git pull
-chmod a+x /home/pirate/raspberrycloud/node/*
-/home/pirate/raspberrycloud/node/publish_status.sh &
+chmod +x /home/pirate/raspberrycloud/node/publish_status.sh
+/home/pirate/raspberrycloud/node/publish_status.sh
